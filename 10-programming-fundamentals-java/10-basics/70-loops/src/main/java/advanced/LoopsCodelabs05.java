d0c65d5b7674b89dd6bb7cc1696cc450a0f0f00b
Klaas Devits <klaas.devits@gmail.com>
2021-09-07 15:09:16 +0200

    loops advanced - codelab 05 done

--- 10-programming-fundamentals-java/10-basics/70-loops/src/main/java/advanced/LoopsCodelabs05.java
+++ 10-programming-fundamentals-java/10-basics/70-loops/src/main/java/advanced/LoopsCodelabs05.java
@@ -12,7 +12,17 @@ public class LoopsCodelabs05 {
      * Tip: go take a look at the Math.min and .max methods.
      */
     public static int minMaxDifference(int[] numbers) {
-        throw new UnsupportedOperationException("Implement me");
+        if (numbers.length < 2) {
+            return 0;
+        }
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            min = Math.min(min, numbers[i]);
+            max = Math.max(max, numbers[i]);
+        }
+        return max - min;
     }
 
 }
