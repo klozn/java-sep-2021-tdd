29b9c95c4ab2a37e880aa744ee8854f3dac8d49a
christophmarckx <christoph.marckx@cegeka.com>
2021-09-07 15:00:21 +0200

    Fixed bug in LoopsCodelabs03Test.java

--- 10-programming-fundamentals-java/10-basics/70-loops/src/test/java/intermediate/LoopsCodelabs03Test.java
+++ 10-programming-fundamentals-java/10-basics/70-loops/src/test/java/intermediate/LoopsCodelabs03Test.java
@@ -13,12 +13,12 @@ class LoopsCodelabs03Test {
 
     @Test
     void simpleMerge() {
-        assertThat(LoopsCodelabs03.merge(new int[]{1, 2, 3}, new int[]{1, 2, 3})).contains(1, 1, 2, 2, 3, 3);
+        assertThat(LoopsCodelabs03.merge(new int[]{1, 2, 3}, new int[]{1, 2, 3})).containsExactly(1, 1, 2, 2, 3, 3);
     }
 
     @Test
     void advancedMerge() {
         assertThat(LoopsCodelabs03.merge(new int[]{3, 5, 15, 236, 78, 45, 95}, new int[]{1, 2, 3, 4, 6, 8, -10}))
-                .contains(3, 1, 5, 2, 15, 3, 236, 4, 78, 6, 45, 8, 95, -10);
+                .containsExactly(3, 1, 5, 2, 15, 3, 236, 4, 78, 6, 45, 8, 95, -10);
     }
 }
