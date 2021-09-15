e24a059bd8859da6e643d08d4789c715e3a1ec63
vercruyssetim <tim.vercruysse@cegeka.be>
2021-09-15 16:12:24 +0200

    update on the encapsulation exercise

--- 10-programming-fundamentals-java/20-oop/10-objects-and-classes/30-classes/src/main/java/codelab11/Butler.java
+++ 10-programming-fundamentals-java/20-oop/10-objects-and-classes/30-classes/src/main/java/codelab11/Butler.java
@@ -12,4 +12,11 @@ public class Butler {
         System.out.println("Butler " + name + " is serving " + food.name + " in the dining room");
         diningroom.serveFood(food);
     }
+
+    public void tendTo(Garden garden) {
+        System.out.println("Gardener " + name + " will (try his best to) tend to the garden.");
+        garden.removeWeeds();
+        garden.trimHedge();
+        garden.mowGrass();
+    }
 }
--- 10-programming-fundamentals-java/20-oop/10-objects-and-classes/30-classes/src/main/java/codelab11/Main.java
+++ 10-programming-fundamentals-java/20-oop/10-objects-and-classes/30-classes/src/main/java/codelab11/Main.java
@@ -12,8 +12,17 @@ public class Main {
      *      2. Encapsulate as much as possible (private if possible, then package private, then protected, if nothing else goes finally public)
      *
      * Bonus (advanced) exercise:
-     *      Avoid using getters as much as possible. It's a sign of feature envy/message chain (code smells).
-     *      https://refactoring.guru/refactoring/smells/couplers
+     *      The gardener became sick suddenly, but the garden needs urgent tending.
+     *      Luckily the butler has some gardening skills.
+     *      How hard would it be to let the butler tend to the gardens?
+     *
+     *      Try to refactor the code in such a way that you don't have any getters or setters.
+     *      Using too many getters or setters is a sign of feature envy/message chain (code smells).
+     *      See: https://refactoring.guru/refactoring/smells/couplers for more information.
+     *
+     *      What would be the benefit in writing your codebase without any getters or setters?
+     *
+     *      After this change how hard is it make the gardener once again responsible for the garden?
      */
     public static void main(String[] args) {
         House house = new House();
