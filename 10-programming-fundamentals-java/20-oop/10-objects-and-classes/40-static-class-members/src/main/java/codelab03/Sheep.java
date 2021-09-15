c7fb1077ded7035f10059551b265dfae5b9e7822
Klaas Devits <klaas.devits@gmail.com>
2021-09-15 08:15:06 +0200

    static class members codelab03 done

--- 10-programming-fundamentals-java/20-oop/10-objects-and-classes/40-static-class-members/src/main/java/codelab03/Sheep.java
+++ 10-programming-fundamentals-java/20-oop/10-objects-and-classes/40-static-class-members/src/main/java/codelab03/Sheep.java
@@ -2,6 +2,7 @@ package codelab03;
 
 public class Sheep {
     private static int amountOfActiveSheep;
+    public final static String ILLEGAL_NICKNAME = "dolly";
 
     private String nickname;
     private double price;
@@ -31,14 +32,14 @@ public class Sheep {
     }
 
     private void setNickname(String nickname) {
-        if (!nickname.strip().equalsIgnoreCase("dolly")) {
+        if (!nickname.strip().equalsIgnoreCase(ILLEGAL_NICKNAME)) {
             this.nickname = nickname;
         }
     }
 
     @Override
     public String toString() {
-        return String.format("I'm a sheep named %s (which is not Dolly) and I'm %s",
-                nickname, white? "white": "not white");
+        return String.format("I'm a sheep named %s (which is not Dolly) and I'm %swhite",
+                nickname, white ? "": "not ");
     }
 }
