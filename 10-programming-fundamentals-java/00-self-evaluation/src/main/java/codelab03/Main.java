e85b14b060611539fc9c35ebf686bda9207a20c5
Klaas Devits <klaas.devits@gmail.com>
2021-09-20 13:55:53 +0200

    Self-Eval codelab03: revised

--- 10-programming-fundamentals-java/00-self-evaluation/src/main/java/codelab03/Main.java
+++ 10-programming-fundamentals-java/00-self-evaluation/src/main/java/codelab03/Main.java
@@ -4,15 +4,16 @@ package codelab03;
  * Read the README.md for the assignment
  */
 public class Main {
+    public static final char SYMBOL = '@';
 
     public static char[][] createArrayForSize(int size) {
         char[][] square = new char[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (i == 0 || i == size - 1) {
-                    square[i][j] = '@';
+                    square[i][j] = SYMBOL;
                 } else if (j == 0 || j == size - 1) {
-                    square[i][j] = '@';
+                    square[i][j] = SYMBOL;
                 } else {
                     square[i][j] = ' ';
                 }
@@ -23,10 +24,13 @@ public class Main {
 
     public static void main(String[] args) {
         char[][] square = createArrayForSize(6);
+        printArray(square);
+    }
 
-        for (int i = 0; i < square.length; i++) {
-            for (int j = 0; j < square.length; j++) {
-                System.out.print(square[i][j]);
+    private static void printArray(char[][] array) {
+        for (char[] chars : array) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(chars[j]);
             }
             System.out.println();
         }
