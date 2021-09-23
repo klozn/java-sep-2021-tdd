d2963edda53e9acae34ac6f5558abdfbf6890a05
Klaas Devits <klaas.devits@gmail.com>
2021-09-23 13:48:16 +0200

    CountFour: removed unnecessary import statement

--- 10-programming-fundamentals-java/20-oop/20-object-oriented/60-combined-codelabs/src/main/java/codelab07/domain/utility/GameEvaluationUtility.java
+++ 10-programming-fundamentals-java/20-oop/20-object-oriented/60-combined-codelabs/src/main/java/codelab07/domain/utility/GameEvaluationUtility.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static codelab07.domain.grid.Grid.*;
 
