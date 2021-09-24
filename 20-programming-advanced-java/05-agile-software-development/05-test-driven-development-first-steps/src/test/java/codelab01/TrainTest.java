cf5473f68e2f629378e8548036b16916cdc239eb
Klaas Devits <klaas.devits@gmail.com>
2021-09-24 10:09:54 +0200

    05TDD: codelab01

--- 20-programming-advanced-java/05-agile-software-development/05-test-driven-development-first-steps/src/test/java/codelab01/TrainTest.java
+++ 20-programming-advanced-java/05-agile-software-development/05-test-driven-development-first-steps/src/test/java/codelab01/TrainTest.java
@@ -21,10 +21,50 @@ class TrainTest {
     }
 
     @Test
-    void givenZeroHoursDelay_whenCustomerSatisfactionCalled_returnRightString() {
+    void givenZeroHourDelay_whenCustomerSatisfactionCalled_thenReturnRightMessage() {
         Train train = new Train();
         train.setHoursDelayed(0);
-        assertEquals("Nice 'n rolling!", train.customerSatisfaction());
+        String message = train.customerSatisfaction();
+        assertEquals("Nice 'n rolling!", message);
+    }
+
+    @Test
+    void givenOneHourDelay_whenCustomerSatisfactionCalled_thenReturnRightMessage() {
+        Train train = new Train();
+        train.setHoursDelayed(1);
+        String message = train.customerSatisfaction();
+        assertEquals("Just another day at the train ...", message);
+    }
+
+    @Test
+    void givenTwoHourDelay_whenCustomerSatisfactionCalled_thenReturnRightMessage() {
+        Train train = new Train();
+        train.setHoursDelayed(2);
+        String message = train.customerSatisfaction();
+        assertEquals("This train is annoying me!", message);
+    }
+
+    @Test
+    void givenThreeHourDelay_whenCustomerSatisfactionCalled_thenReturnRightMessage() {
+        Train train = new Train();
+        train.setHoursDelayed(3);
+        String message = train.customerSatisfaction();
+        assertEquals("B-u-u-urn it!", message);
+    }
+
+    @Test
+    void givenNegativeHourDelay_whenCustomerSatisfactionCalled_thenReturnZeroHourMessage() {
+        Train train = new Train();
+        train.setHoursDelayed(-1);
+        String message = train.customerSatisfaction();
+        assertEquals("Nice 'n rolling!", message);
+    }
+
+    @Test
+    void givenMoreThanThreeHourDelay_whenCustomerSatisfactionCalled_thenThrowIllegalArgumentException() {
+        Train train = new Train();
+        train.setHoursDelayed(4);
+        assertThrows(IllegalArgumentException.class, train::customerSatisfaction);
     }
 
 }
