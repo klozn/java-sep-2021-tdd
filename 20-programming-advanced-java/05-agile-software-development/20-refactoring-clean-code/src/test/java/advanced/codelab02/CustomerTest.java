707906b87c4d33f809be753521b1d54f97b0430f
Klaas Devits <klaas.devits@gmail.com>
2021-09-28 08:15:57 +0200

    Refactoring advanced codelab02 - fixed the tests

--- 20-programming-advanced-java/05-agile-software-development/20-refactoring-clean-code/src/test/java/advanced/codelab02/CustomerTest.java
+++ 20-programming-advanced-java/05-agile-software-development/20-refactoring-clean-code/src/test/java/advanced/codelab02/CustomerTest.java
@@ -12,10 +12,11 @@ class CustomerTest {
                 new Rental[]{
                         new Rental(new Movie("Minority Report", Movie.REGULAR), 3)
                 });
-        String expectedResult = String.format("Rental Record for Becky%n" +
-                "\tMinority Report\t3.5%n" +
-                "Amount owed is 3.5%n" +
-                "You earned 1 frequent renter points");
+        String expectedResult = """
+                Rental Record for Becky
+                \tMinority Report\t3.5
+                Amount owed is 3.5
+                You earned 1 frequent renter points""";
 
         String actualResult = customer.statement();
 
@@ -36,10 +37,11 @@ class CustomerTest {
                 new Rental[]{
                         new Rental(new Movie("A Quiet Place", Movie.NEW_RELEASE), 3)
                 });
-        String expectedResult = String.format("Rental Record for Bruce%n" +
-                "\tA Quiet Place\t9.0%n" +
-                "Amount owed is 9.0%n" +
-                "You earned 2 frequent renter points");
+        String expectedResult = """
+                Rental Record for Bruce
+                \tA Quiet Place\t9.0
+                Amount owed is 9.0
+                You earned 2 frequent renter points""";
 
         String actualResult = customer.statement();
 
@@ -52,10 +54,11 @@ class CustomerTest {
                 new Rental[]{
                         new Rental(new Movie("Wall-E", Movie.CHILDRENS), 3)
                 });
-        String expectedResult = String.format("Rental Record for Lil' Timmy%n" +
-                "\tWall-E\t1.5%n" +
-                "Amount owed is 1.5%n" +
-                "You earned 1 frequent renter points");
+        String expectedResult = """
+                Rental Record for Lil' Timmy
+                \tWall-E\t1.5
+                Amount owed is 1.5
+                You earned 1 frequent renter points""";
 
         String actualResult = customer.statement();
 
@@ -70,12 +73,13 @@ class CustomerTest {
                         new Rental(new Movie("Game Night", Movie.NEW_RELEASE), 2),
                         new Rental(new Movie("The theory of everything", Movie.REGULAR), 8),
                 });
-        String expectedResult = String.format("Rental Record for Suzzy%n" +
-                "\tMonsters, inc.\t6.0%n" +
-                "\tGame Night\t6.0%n" +
-                "\tThe theory of everything\t11.0%n" +
-                "Amount owed is 23.0%n" +
-                "You earned 4 frequent renter points");
+        String expectedResult = """
+                Rental Record for Suzzy
+                \tMonsters, inc.\t6.0
+                \tGame Night\t6.0
+                \tThe theory of everything\t11.0
+                Amount owed is 23.0
+                You earned 4 frequent renter points""";
 
         String actualResult = customer.statement();
 
