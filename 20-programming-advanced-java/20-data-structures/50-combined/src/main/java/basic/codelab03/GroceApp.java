c8bebdd6b141d715f3a078193f43cef90feae0a0
Klaas Devits <klaas.devits@gmail.com>
2021-09-30 20:01:25 +0200

    DataStructures combined basic codelab3 improved readability

--- 20-programming-advanced-java/20-data-structures/50-combined/src/main/java/basic/codelab03/GroceApp.java
+++ 20-programming-advanced-java/20-data-structures/50-combined/src/main/java/basic/codelab03/GroceApp.java
@@ -7,38 +7,34 @@ import java.util.Scanner;
 public class GroceApp {
     public static void main(String[] args) {
         Rack rack = new Rack(List.of(new Shelf(), new Shelf(), new Shelf(), new Shelf()));
-        rack.fill(List.of(
-                new Grocery("apple"),
-                new Grocery("pear"),
-                new Grocery("orange"),
-                new Grocery("apple"),
-                new Grocery("pear")), 0);
+        rack.fill(List.of(new Grocery("apple"), new Grocery("pear"), new Grocery("orange"),
+                        new Grocery("apple"), new Grocery("pear")), 0);
         rack.fill(List.of(new Grocery("ham"), new Grocery("bacon")), 1);
         rack.fill(List.of(new Grocery("vegi ham"), new Grocery("vegi bacon")), 2);
-        rack.fill(List.of(
-                new Grocery("nintendo"),
-                new Grocery("playstation"),
-                new Grocery("pc"),
-                new Grocery("xbox")), 3);
-
+        rack.fill(List.of(new Grocery("nintendo"), new Grocery("playstation"),
+                        new Grocery("pc"), new Grocery("xbox")), 3);
         do {
             System.out.println(rack);
             int input = askIndex();
             Grocery grocery = getGrocery(rack, input);
             System.out.println("You got: " + grocery.getName() + ".");
+
         } while (!rack.isEmpty());
+
         System.out.println("Do you need a bag?");
     }
 
     private static Grocery getGrocery(Rack rack, int index) {
         try {
             return rack.takeGroceryFromShelf(index);
-        } catch (IllegalStateException e) {
+        }
+        catch (IllegalStateException e) {
             System.out.println(e.getMessage());
             System.out.println("Shelf " + index + " is empty.");
             int input = askIndex();
             return getGrocery(rack, input);
-        } catch (IllegalArgumentException e) {
+        }
+        catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             int input = askIndex();
             return getGrocery(rack, input);
