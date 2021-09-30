a4bad77f850fc88f7c4366e9e4d77f6ab885f1f9
Klaas Devits <klaas.devits@gmail.com>
2021-09-30 14:52:08 +0200

    exception handling advanced codelab done

--- 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/advanced/codelab01/Calculator.java
+++ 20-programming-advanced-java/10-exceptions/10-exception-handling/src/main/java/advanced/codelab01/Calculator.java
@@ -1,5 +1,6 @@
 package advanced.codelab01;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
@@ -17,16 +18,30 @@ public class Calculator {
         System.out.println("================================");
         System.out.println("================================");
 
+        int numerator = askFor("numerator");
+        int denominator = askFor("denominator");
+
+        try {
+            var result = CalculatorService.divide(numerator, denominator);
+            System.out.println("The quotient of the division is: " + result);
+        } catch (ArithmeticException ae) {
+            System.err.println("Could not divide " + numerator + " by " + denominator);
+        }
+    }
+
+    private static int askFor(String numberType) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Provide a numerator (int): ");
-        int numerator = in.nextInt();
-
-        System.out.print("Provide a denominator (int): ");
-        in.nextLine();
-        int denominator = in.nextInt();
-
-        var result = CalculatorService.divide(numerator, denominator);
-        System.out.println("The quotient of the division is: " + result);
+        System.out.print("Provide a " + numberType + " (int): ");
+        int number;
+        try {
+            number = in.nextInt();
+        } catch (InputMismatchException ime) {
+            number = numberType.equals("nominator") ? 10 : 5;
+            System.out.println("\n(invalid " + numberType + " provided. Therefore it is set to value " + number + ")");
+            System.out.println("Please try again");
+            return askFor(numberType);
+        }
+        return number;
     }
 
 }
