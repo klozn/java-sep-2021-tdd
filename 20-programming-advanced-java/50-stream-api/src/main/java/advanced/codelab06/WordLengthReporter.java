130d6303d5c47cb5a98419e91689c60626a6c429
Klaas Devits <klaas.devits@gmail.com>
2021-10-06 15:29:15 +0200

    improved readability

--- 20-programming-advanced-java/50-stream-api/src/main/java/advanced/codelab06/WordLengthReporter.java
+++ 20-programming-advanced-java/50-stream-api/src/main/java/advanced/codelab06/WordLengthReporter.java
@@ -7,13 +7,17 @@ import java.util.stream.Collectors;
 public class WordLengthReporter {
 
     public String reportAmountOfWordsPerWordLength(String sentence) {
-        final StringJoiner sj = new StringJoiner(", ");
 
-        final var stringsPerLength = Arrays.stream(sentence.split(" "))
-                .collect(Collectors.groupingBy(String::length, Collectors.toList()));
+        final var sj = new StringJoiner(", ");
+
+        final var stringsPerLength =
+                Arrays.stream(sentence.split(" "))
+                        .collect(Collectors.groupingBy(String::length, Collectors.toList()));
+
         stringsPerLength.keySet().stream()
                 .sorted()
                 .forEach(key -> sj.add(key + ":" + stringsPerLength.get(key).size()));
+
         return sj.toString();
     }
 }
