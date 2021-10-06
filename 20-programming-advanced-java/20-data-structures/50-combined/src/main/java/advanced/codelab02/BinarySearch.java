d1f5b26b12f7f291d566b1cd16894898266ed94c
Klaas Devits <klaas.devits@gmail.com>
2021-10-06 10:28:29 +0200

    improved readability

--- 20-programming-advanced-java/20-data-structures/50-combined/src/main/java/advanced/codelab02/BinarySearch.java
+++ 20-programming-advanced-java/20-data-structures/50-combined/src/main/java/advanced/codelab02/BinarySearch.java
@@ -4,22 +4,28 @@ import java.util.List;
 
 public class BinarySearch {
 
-    <T extends Comparable<T>> boolean isElementInList(T elementToSearch, List<T> listToSearchFrom) {
+    <T extends Comparable<? super T>>
+    boolean isElementInList(T elementToSearch, List<T> listToSearchFrom) {
         int listSize = listToSearchFrom.size();
         double leftIndex = 0;
         double rightIndex = listSize - 1;
+
         while (leftIndex <= rightIndex) {
+
             int index = (int) Math.floor((leftIndex + rightIndex)/2);
             T current = listToSearchFrom.get(index);
-            if (current.compareTo(elementToSearch) < 0) {
+            int compareValue = current.compareTo(elementToSearch);
+
+            if (compareValue < 0) {
                 leftIndex = index + 1;
-            } else if (current.compareTo(elementToSearch) > 0) {
+            }
+            else if (compareValue > 0) {
                 rightIndex = index - 1;
-            } else {
+            }
+            else {
                 return true;
             }
         }
         return false;
     }
-
 }
