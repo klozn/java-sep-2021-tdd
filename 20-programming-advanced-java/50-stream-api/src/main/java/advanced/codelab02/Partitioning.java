f2c9a7117805043e5a54281a56f943689161bfc9
Klaas Devits <klaas.devits@gmail.com>
2021-10-06 13:37:18 +0200

    streams advanced codelab02 done

--- 20-programming-advanced-java/50-stream-api/src/main/java/advanced/codelab02/Partitioning.java
+++ 20-programming-advanced-java/50-stream-api/src/main/java/advanced/codelab02/Partitioning.java
@@ -2,10 +2,12 @@ package advanced.codelab02;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Partitioning {
 
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
-        throw new UnsupportedOperationException("Implement me");
+        return people.stream()
+                .collect(Collectors.groupingBy(person -> person.getAge() >= 18, Collectors.toList()));
     }
 }
