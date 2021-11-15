8928b589eb14bc1a8b561ef9fb8c39415f87259c
Klaas Devits <klaas.devits@gmail.com>
2021-11-15 13:54:52 +0100

    tdd-mocking-stubbing codelab02 starting with integration test

---
+++ 20-programming-advanced-java/05-agile-software-development/15-tdd-mocking-stubbing/src/main/java/codelab02/external/NasdaqStockPriceService.java
@@ -0,0 +1,14 @@
+package codelab02.external;
+
+public class NasdaqStockPriceService implements ExternalStockPriceService {
+
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public StockPriceInfoDto fetchCurrentPrice(String stockId) {
+        return null;
+    }
+}
---
+++ 20-programming-advanced-java/05-agile-software-development/15-tdd-mocking-stubbing/src/test/java/codelab02/StockServiceIntegrationTest.java
@@ -0,0 +1,16 @@
+package codelab02;
+
+import codelab02.external.ExternalStockPriceService;
+import org.junit.jupiter.api.BeforeEach;
+
+public class StockServiceIntegrationTest {
+
+    private StockService stockService;
+    private ExternalStockPriceService externalStockPriceService;
+    private StockMapper stockMapper;
+
+    @BeforeEach
+    void setUp() {
+        stockService = new
+    }
+}
